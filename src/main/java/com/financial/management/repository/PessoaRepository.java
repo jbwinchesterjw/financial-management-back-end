607d0cdda29aae2e518ef10f0b6b13d3244d9199
jbwinchesterjw <49697170+jbwinchesterjw@users.noreply.github.com>
2022-10-23 08:36:02 -0300

    feature/fm-b-01 - Alteração de script e criação do método findByNomeContaining

--- src/main/java/com/financial/management/repository/PessoaRepository.java
+++ src/main/java/com/financial/management/repository/PessoaRepository.java
@@ -1,10 +1,14 @@
 package com.financial.management.repository;
 
 import com.financial.management.model.Pessoa;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-    
+
+    Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
+
 }
