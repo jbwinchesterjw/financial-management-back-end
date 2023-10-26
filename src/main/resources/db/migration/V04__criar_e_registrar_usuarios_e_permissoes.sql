48d787515941bdfa57f7e3bae8c3ba78c6b13c5c
jbwinchesterjw <jbwincheaster@gmail.com>
2023-10-26 13:21:04 -0300

    Alterando senha2°

--- src/main/resources/db/migration/V04__criar_e_registrar_usuarios_e_permissoes.sql
+++ src/main/resources/db/migration/V04__criar_e_registrar_usuarios_e_permissoes.sql
@@ -19,7 +19,7 @@ CREATE TABLE usuario_permissao (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO usuario (codigo, nome, email, senha) values (1, 'Administrador', 'admin@financialmanagement.com', '$2a$10$X607ZPhQ4EgGNaYKt3n4SONjIv9zc.VMWdEuhCuba7oLAL5IvcL5.');
-INSERT INTO usuario (codigo, nome, email, senha) values (2, 'Joao silva', 'joao@financialmanagement.com', '$2a$10$BNv17IeR9AbGFL2GsSvOueGoZYU66cDhTSlMeGh0OYBkm.kvWYNCe');
+INSERT INTO usuario (codigo, nome, email, senha) values (2, 'Joao silva', 'joao@financialmanagement.com', '$2a$10$6/1iSFK0PPXYlfHwEg6.BuvJ4u/Tcpo.Q64Pvy2nxmizvgM0UYY6C');
 
 INSERT INTO permissao (codigo, descricao) values (1, 'ROLE_CADASTRAR_CATEGORIA');
 INSERT INTO permissao (codigo, descricao) values (2, 'ROLE_PESQUISAR_CATEGORIA');
