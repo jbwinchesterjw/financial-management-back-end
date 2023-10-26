18eda5aa67e062f05dcd0d2ad9a39bce475484c3
jbwinchesterjw <jbwincheaster@gmail.com>
2023-10-26 13:43:29 -0300

    Deploy projeto produção

--- src/main/resources/db/migration/V04__criar_e_registrar_usuarios_e_permissoes.sql
+++ src/main/resources/db/migration/V04__criar_e_registrar_usuarios_e_permissoes.sql
@@ -19,7 +19,7 @@ CREATE TABLE usuario_permissao (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO usuario (codigo, nome, email, senha) values (1, 'Administrador', 'admin@financialmanagement.com', '$2a$10$X607ZPhQ4EgGNaYKt3n4SONjIv9zc.VMWdEuhCuba7oLAL5IvcL5.');
-INSERT INTO usuario (codigo, nome, email, senha) values (2, 'Joao silva', 'joao@financialmanagement.com', '$2a$10$6/1iSFK0PPXYlfHwEg6.BuvJ4u/Tcpo.Q64Pvy2nxmizvgM0UYY6C');
+INSERT INTO usuario (codigo, nome, email, senha) values (2, 'Joao silva', 'joao@financialmanagement.com', '$2a$10$LriBhz05PC43rX9kfqZsdeijbo5wFb63hAHU7s9dkZX30uoxigpFO');
 
 INSERT INTO permissao (codigo, descricao) values (1, 'ROLE_CADASTRAR_CATEGORIA');
 INSERT INTO permissao (codigo, descricao) values (2, 'ROLE_PESQUISAR_CATEGORIA');
